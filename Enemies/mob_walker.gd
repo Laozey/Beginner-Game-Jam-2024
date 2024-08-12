a17eefe8837a551cd9b78d99fda400a785972e27
TheBUSHMAN <114523323+TheBUSHMAN@users.noreply.github.com>
2024-08-12 19:28:27 +0100

    Enemy, TestLevel + Spawner
    Check ReadMe, nothing should get in the way of code as it's all new stuff including an enemy

---
+++ Enemies/mob_walker.gd
@@ -0,0 +1,20 @@
+extends CharacterBody2D
+
+@export var health = 30 
+@export var speed = 100
+
+@onready var player = get_node("/root/TestLevel/Player") #Will need to change the root they get
+
+func _ready():
+	return
+	
+
+func _physics_process(delta):
+	var direction = global_position.direction_to(player.global_position)
+	velocity = direction * speed
+	move_and_slide()
+
+func take_damage(damage):
+		health -= damage
+		if health <= 0:
+			queue_free()
--- UI_UX/TitleScreen.gd
+++ UI_UX/TitleScreen.gd
@@ -4,7 +4,7 @@ func _ready():
 	MusicPlayer.play_music_title()
 
 func _on_start_button_pressed():
-	get_tree().change_scene_to_file("res://Player/player.tscn") #Put the Game Scene Here
+	get_tree().change_scene_to_file("res://Levels/test_level.gd") #Put the Game Scene Here
 
 
 func _on_quit_button_pressed():
