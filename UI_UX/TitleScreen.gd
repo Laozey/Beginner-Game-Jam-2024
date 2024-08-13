2582662f5be57743aa8a4a03758cca4d18abc33c
TheBUSHMAN <114523323+TheBUSHMAN@users.noreply.github.com>
2024-08-13 15:39:36 +0100

    Update TitleScreen.gd

--- UI_UX/TitleScreen.gd
+++ UI_UX/TitleScreen.gd
@@ -4,12 +4,10 @@ func _ready():
 	MusicPlayer.play_music_title()
 
 func _on_start_button_pressed():
-	get_tree().change_scene_to_file("res://Levels/test_level.gd") #Put the Game Scene Here
-
+	get_tree().change_scene_to_file("res://Levels/test_level.tscn") #Put the Game Scene Here
 
 func _on_quit_button_pressed():
 	get_tree().quit()
-
 
 func _on_credit_button_pressed():
 	get_tree().change_scene_to_file("res://UI_UX/credits.tscn")
